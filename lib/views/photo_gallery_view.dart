af02da4073918798c15f1fbe0af4a3f0669b8967
Silas Almeida <silas.ms.almeida@gmail.com>
2023-01-10 10:59:05 -0300

    implements photo_gallery_view component

--- lib/views/photo_gallery_view.dart
+++ lib/views/photo_gallery_view.dart
@@ -1,1 +1,47 @@
+import 'package:bloc_firebase_gallery/bloc/app_bloc.dart';
+import 'package:bloc_firebase_gallery/bloc/app_event.dart';
+import 'package:bloc_firebase_gallery/bloc/app_state.dart';
+import 'package:bloc_firebase_gallery/views/main_popup_menu_button.dart';
+import 'package:bloc_firebase_gallery/views/storage_image_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:image_picker/image_picker.dart';
+
+class PhotoGalleryView extends HookWidget {
+  const PhotoGalleryView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final imagePicker = useMemoized(() => ImagePicker(), [key]);
+    final images = context.watch<AppBloc>().state.images ?? [];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Photo Gallery'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              imagePicker.pickImage(source: ImageSource.gallery).then((image) {
+                if (image == null) return;
+                context.read<AppBloc>().add(
+                      AppEventUploadImage(
+                        filePathToUpload: image.path,
+                      ),
+                    );
+              });
+            },
+            icon: const Icon(Icons.upload),
+          ),
+          const MainPopupMenuButton(),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(8.0),
+        mainAxisSpacing: 20.0,
+        crossAxisSpacing: 20.0,
+        children: images.map((img) => StorageImageView(image: img)).toList(),
+      ),
+    );
+  }
+}
